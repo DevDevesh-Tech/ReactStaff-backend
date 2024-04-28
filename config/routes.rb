4031f58c9280ed017a8c96bc2ec14bf383f66dc4
devesh140901 <deveshkumarsingh99@gmail.com>
2024-04-28 15:38:11 +0530

    api setup

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,16 @@ Rails.application.routes.draw do
     end
   end
 
+  namespace :api do
+    namespace :v1 do
+      resources :employees do
+        collection do
+          post :import
+        end
+      end
+    end
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
